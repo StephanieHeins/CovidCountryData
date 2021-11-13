dda2c6838494e1879e70d2915610c88cdd56f5a8
Steph Heins <stephanie_heins@yahoo.com>
2021-11-12 20:56:32 -0800

    Got the search function to work & return data

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,9 @@ const APIKey = "7da14da879msh47cc8472a21896cp170670jsn11ea31ce2335"
 // Search CC function 
 
 function ccSearch(event) {
-    fetch("https://covid-19-data.p.rapidapi.com/country/code?code=us&apikey=7da14da879msh47cc8472a21896cp170670jsn11ea31ce2335", {
+    let userInput=$("#searchInput").val().trim();
+    var Query = 'https://covid-19-data.p.rapidapi.com/country/code?code=' + userInput + '&apikey=7da14da879msh47cc8472a21896cp170670jsn11ea31ce2335'
+    fetch(Query, {
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
@@ -22,7 +24,6 @@ function ccSearch(event) {
         console.error(err);
     });
 }
-
 
 
 // Onclick handlers
