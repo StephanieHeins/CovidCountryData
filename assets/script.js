1d4a75bc8765fe930c94e0229d79a6a5a86efdf0
Steph Heins <stephanie_heins@yahoo.com>
2021-11-12 19:15:15 -0800

    Added some HTML to index.html for display, and got API in script.js working and returning data

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,29 @@
-fetch("https://covid-19-data.p.rapidapi.com/country/code?code=us&apikey=7da14da879msh47cc8472a21896cp170670jsn11ea31ce2335", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "covid-19-data.p.rapidapi.com",
-		"x-rapidapi-key": "7da14da879msh47cc8472a21896cp170670jsn11ea31ce2335"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
+// API Key
+const APIKey = "7da14da879msh47cc8472a21896cp170670jsn11ea31ce2335"
+
+// Global Variables
+
+
+// Search CC function 
+
+function ccSearch(event) {
+    fetch("https://covid-19-data.p.rapidapi.com/country/code?code=us&apikey=7da14da879msh47cc8472a21896cp170670jsn11ea31ce2335", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+            "x-rapidapi-key": "7da14da879msh47cc8472a21896cp170670jsn11ea31ce2335"
+        }
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+        console.log(responseJson);
+    })
+    .catch(err => {
+        console.error(err);
+    });
+}
+
+
+
+// Onclick handlers
+$("#searchBtn").click(ccSearch);
